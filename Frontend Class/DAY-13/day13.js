8c55dced639b086908417c0cf8233e921dbf11e6
Sneha <snehajha08112000@gmail.com>
2021-07-07 17:26:20 +0530

    Day13 class updated

--- Frontend Class/DAY-13/day13.js
+++ Frontend Class/DAY-13/day13.js
@@ -27,16 +27,34 @@
 //fetch api
 // console.log();
 // fetch('https://api.github.com/users/sneha-jha08')
-// .then(res => (res.json())
-//     .then(data => console.log(data.name)))
+//     .then(res => (res.json())
+//         .then(data => {
+//                 console.log("User Login: " + data.login);
+//                 console.log("User URL: " + data.html_url);
+//                 console.log("UserName: " + data.name)
+// }))
+
+//data.login
+//data.html_url
+//data.name
 // .catch(error => console.log(error))
 // .then(res => console.log(res.ok))
 // .then(data => (console.log(data.json())))
 
-async function hello() {
-    // console.log('hello world');
-    return "hello"
-}
-var a = hello();
-console.log(a);
-a.then(data => console.log(data))
+// async function hello() {
+//     // console.log('hello world');
+//     // return "Hello"
+//     const res = await fetch('https://api.github.com/users/sneha-jha08');
+//     console.log(res);
+// }
+// var a = hello();
+// console.log(a);
+// a.then(data => console.log(data))
+
+// var User_Login = data.login;
+// var User_URL = data.html_url;
+// var UserName = data.name;
+// div.innerHTML = "Username: " + UserName + "< br >" + "User Login ID: " + User_Login + "< br >" + "User HTML URL " + User_URL;
+// console.log("User Login: " + data.login);
+// console.log("User URL: " + data.html_url);
+// console.log("UserName: " + data.name)
