980175dac0d83c24282239bc5edc2ff096f632eb
Sneha <snehajha08112000@gmail.com>
2021-07-11 18:39:47 +0530

    Day17 class
    incomplete

---
+++ Frontend Class/DAY-17/my-app/src/App.jsx
@@ -0,0 +1,23 @@
+// import logo from './logo.svg';
+import React from 'react';
+import './App.css';
+import Card from './components/Card';
+
+function App(props) {
+    console.log(props);
+//     const title="page title";
+//     const paragraph="page";
+//     const page={
+// title:"Page Title",
+// paragraph:"page "
+// // };
+// let a =10;
+// let b=20;
+
+  return (<div className="App" >
+    <Card/>
+</div>);
+  
+};
+
+export default App;
---
+++ Frontend Class/DAY-17/my-app/src/components/Card.jsx
@@ -0,0 +1,12 @@
+const Card=()=>{
+return(
+<div>
+    {/* <img src="" alt=""></img> */}
+    <div className="container">
+        <div className="title"><title></title></div>
+        <button></button>
+    </div>
+</div>);
+}
+
+export default Card;
