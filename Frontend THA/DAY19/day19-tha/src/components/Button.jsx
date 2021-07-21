28f852f7af36e92492cea0f7ccb73e1ae9c8a104
Sneha <snehajha08112000@gmail.com>
2021-07-21 17:50:33 +0530

    added comments

--- Frontend THA/DAY19/day19-tha/src/components/Button.jsx
+++ Frontend THA/DAY19/day19-tha/src/components/Button.jsx
@@ -1,14 +1,18 @@
+//since we cant update the values direectly to the DOM so we use hooks in order to do so
+//over here were using useState to update the values
+
+
 import "./Button.css"
 import React, { useState } from "react";
 function Button(props) {
-  let [count, setCount] = useState(0);
+  let [count, setCount] = useState(0);// deconstructing the arrays and assigning  count =0;
 
-  function increment() {
+  function increment() {// made a new function to increment the values
     let newCount = count + 1;
-    setCount(newCount);
+    setCount(newCount);//setting the value of count= newcount
     console.log(props.id+" "+(newCount));
   }return(
-  <button onClick={increment}>{count}</button>)
+  <button onClick={increment}>{count}</button>)//shows updated value of count ie newCount
 }
 
 export default Button;
